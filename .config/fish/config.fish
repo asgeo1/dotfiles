018b309ac72528a4b06c1f54c3ce79c6bbc1680a
Adam George <github@adamgeorge.com>
2019-02-26 09:45:36 +1100

    Updating fish config with some android bits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,3 +29,19 @@ set -g fish_user_paths "/usr/local/opt/postgresql@10/bin" $fish_user_paths
 
 # mysql 5.7 needed for fieldfolio
 set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
+
+# customise the path
+set -x PATH "$HOME/Projects/dotfiles/bin" $PATH
+set -x PATH "$HOME/Development/Android/sdk/tools" $PATH
+set -x PATH "$HOME/Development/Android/sdk/platform-tools" $PATH
+set -x PATH "$HOME/Development/Android/ndk" $PATH
+set -x PATH "$HOME/.node/bin" $PATH
+set -x PATH "$HOME/.rbenv/bin" $PATH
+set -x PATH "./node_modules/.bin" $PATH
+set -x PATH "$HOME/Development/flutter/bin" $PATH
+
+set -x ANDROID_HOME "$HOME/Development/Android/sdk"
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home"
+
+# for tmuxinator:
+set -x DISABLE_AUTO_TITLE true
