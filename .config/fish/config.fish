66b9c4146f6784ea1041799144bae183e95d4f5f
Adam George <github@adamgeorge.com>
2019-10-17 10:18:35 +1100

    Serverless completions for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,3 +53,13 @@ set -x LANG en_AU.UTF-8
 
 # completions for kitty
 kitty + complete setup fish | source
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/asgeo1/.node/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/asgeo1/.node/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/asgeo1/.node/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /Users/asgeo1/.node/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /Users/asgeo1/.node/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /Users/asgeo1/.node/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
