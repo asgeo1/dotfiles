c6b44e0e4524328c500cce959ae360d5c24effe6
Adam George <github@adamgeorge.com>
2021-07-05 13:52:45 +1000

    Add which-key to neovim

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -169,6 +169,17 @@ packer.startup(
 
         use 'axelf4/vim-strip-trailing-whitespace'
 
+        use {
+          "folke/which-key.nvim",
+          config = function()
+            require("which-key").setup {
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+            }
+          end
+        }
+
         -- Neat, but has performance issue
         -- use 'jeffkreeftmeijer/vim-numbertoggle'
     end
