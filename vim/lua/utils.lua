8d956f93da8a4766e62efd8ef02612b5eb8ce2c9
Adam George <github@adamgeorge.com>
2021-07-06 11:47:15 +1000

    Add a function for checking if a module is available or not

--- vim/lua/utils.lua
+++ vim/lua/utils.lua
@@ -140,4 +140,19 @@ M.wrap_lines = function(input, width)
     return output
 end
 
+function IsModuleAvailable(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == 'function' then
+        package.preload[name] = loader
+        return true
+      end
+    end
+    return false
+  end
+end
+
 return M
