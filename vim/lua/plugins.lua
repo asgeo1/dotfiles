b24517e4c5166e8d60ca4ce51c9bfbfdc4233bb7
Adam George <github@adamgeorge.com>
2021-10-03 18:57:56 +1100

    Add mappings for splits/tabs for vim-spectre

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -246,6 +246,28 @@ packer.startup(
  end
 )
 
+if IsModuleAvailable("spectre.actions") then
+  require("spectre").setup {
+    mapping={
+      ['open_in_vsplit'] = {
+          map = "<c-v>",
+          cmd = "<cmd>lua vim.cmd('vsplit ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+          desc = "open in vertical split"
+      },
+      ['open_in_split'] = {
+          map = "<c-s>",
+          cmd = "<cmd>lua vim.cmd('split ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+          desc = "open in horizontal split"
+      },
+      ['open_in_tab'] = {
+          map = "<c-t>",
+          cmd = "<cmd>lua vim.cmd('tab split ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+          desc = "open in new tab"
+      },
+    }
+  }
+end
+
 if IsModuleAvailable("telescope.actions") then
   local actions = require('telescope.actions')
   require("telescope").setup {
