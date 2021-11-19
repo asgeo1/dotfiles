c40c2d4914e52d2acaf8c708c29a708ff5f2eb40
Adam George <github@adamgeorge.com>
2021-11-19 13:58:36 +1100

    Notes on vim LSP dependencies

--- vim/init.lua
+++ vim/init.lua
@@ -7,3 +7,42 @@ require "mappings"
 
 require "lists".setup()
 require "autocmds"
+
+-- DEPENDENCIES:
+--
+-- go install golang.org/x/tools/gopls@latest
+-- pip install 'python-language-server[all]'
+-- npm install -g typescript-language-server
+-- npm install -g vim-language-server
+-- npm install -g vscode-json-languageserver
+-- npm i -g sql-language-server
+-- npm install --global vscode-css-languageserver-bin
+-- npm install --global vscode-html-languageserver-bin
+-- npm i -g bash-language-server
+-- npm install -g dockerfile-language-server-nodejs
+-- gem install solargraph
+-- brew install efm-langserver
+-- yarn global add yaml-language-server
+-- brew install hashicorp/tap/terraform-ls
+-- sudo npm install -g pyright
+-- npm i -g sql-language-server
+-- npm i -g vscode-langservers-extracted
+-- npm install -g vscode-json-languageserver
+-- npm install -g yaml-language-server
+--
+--
+-- git clone https://github.com/sumneko/lua-language-server
+-- cd lua-language-server
+-- git submodule update --init --recursive
+-- cd 3rd/luamake
+-- ./compile/install.sh
+-- cd ../..
+-- ./3rd/luamake/luamake rebuild
+--
+--
+-- $ cd ~/home/you/somewhere
+-- $ git clone git@github.com:phpactor/phpactor
+-- $ cd phpactor
+-- $ composer install
+-- $ cd /usr/local/bin
+-- $ sudo ln -s ~/your/projects/phpactor/bin/phpactor phpactor
