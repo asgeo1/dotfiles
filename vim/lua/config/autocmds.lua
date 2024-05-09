b702ee153de229bfcf5644009b93fa5627fa7211
Adam George <github@adamgeorge.com>
2024-05-09 12:09:40 +1000

    Make sure rbi and .env files have syntax applied

--- vim/lua/config/autocmds.lua
+++ vim/lua/config/autocmds.lua
@@ -13,3 +13,9 @@ vim.cmd [[autocmd FileType floggraph map <buffer> <silent> q <Plug>(FlogQuit)]]
 -- not working:
 vim.cmd [[autocmd FileType floggraph let b:strip_trailing_whitespace_enabled=0]]
 vim.cmd [[augroup END]]
+
+vim.cmd [[augroup custom_ruby_syntax]]
+vim.cmd 'autocmd BufNewFile,BufRead *.rbi set syntax=ruby'
+vim.cmd 'autocmd BufNewFile,BufRead *.rbi set filetype=ruby'
+vim.cmd 'autocmd BufNewFile,BufRead *.env.* set filetype=sh'
+vim.cmd [[augroup END]]
