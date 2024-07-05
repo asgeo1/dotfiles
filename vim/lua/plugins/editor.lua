82bc2ab71f2e661120d2b052b3a0034479ef21bd
Adam George <github@adamgeorge.com>
2024-07-05 11:47:16 +1000

    Trouble API changed, update accordingly

--- vim/lua/plugins/editor.lua
+++ vim/lua/plugins/editor.lua
@@ -54,13 +54,33 @@ return {
     keys = {
       {
         '<leader>xx',
-        '<cmd>TroubleToggle document_diagnostics<cr>',
+        '<cmd>Trouble diagnostics toggle<cr>',
         desc = 'Document Diagnostics (Trouble)',
       },
       {
         '<leader>xX',
-        '<cmd>TroubleToggle workspace_diagnostics<cr>',
-        desc = 'Workspace Diagnostics (Trouble)',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
       },
     },
   },
