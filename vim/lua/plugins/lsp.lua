4eb7574f7ca71755df27a880e7337b643eeb4e98
Adam George <github@adamgeorge.com>
2025-03-10 13:34:35 +1100

    Some LSP plugins updates

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -68,33 +68,50 @@ return {
   -- in real-time right from your editor
   'github/copilot.vim',
 
-  -- ChatGPT plugin for NeoVim
-  --
-  -- Note that session data is stored in ~/.local/state/nvim/chatgpt/
   {
-    'jackMort/ChatGPT.nvim',
+    'yetone/avante.nvim',
     event = 'VeryLazy',
-    config = function()
-      require('chatgpt').setup {
-        -- Don't use this until this issue is resolved: https://github.com/jackMort/ChatGPT.nvim/issues/461
-        -- api_key_cmd = 'op read op://private/OpenAI-NeoVim-ChatGPT/credential --no-newline',
-
-        openai_params = {
-          model = 'gpt-4o',
-        },
-        openai_edit_params = {
-          model = 'gpt-4o',
-        },
-        popup_input = {
-          submit = '<C-t>',
-        },
-      }
-    end,
+    lazy = false,
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-      'MunifTanjim/nui.nvim',
+      'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
-      'folke/trouble.nvim',
-      'nvim-telescope/telescope.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
     },
   },
 
