e065735ffec9005f140a5650150746cdae4a36f3
Adam George <github@adamgeorge.com>
2025-10-02 15:48:45 +1000

    Don't show branch name

--- vim/lua/plugins/ui.lua
+++ vim/lua/plugins/ui.lua
@@ -96,7 +96,7 @@ return {
         },
         sections = {
           lualine_a = { 'mode' },
-          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_b = { 'diff', 'diagnostics' },
           lualine_c = { custom_filename },
           lualine_x = { 'encoding', 'fileformat', 'filetype' },
           lualine_y = { 'progress' },
